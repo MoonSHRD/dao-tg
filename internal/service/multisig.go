7d53fc48aeb5b36b136a0b5bbbb59094c07cf472
Anton Zavodchikov <terisbackno@gmail.com>
2022-05-26 23:16:06 +0500

    Handle new transactions and conficts

--- internal/service/multisig.go
+++ internal/service/multisig.go
@@ -64,9 +64,43 @@ func (service *Service) FormatMultisigTransactions(subscription models.Subscript
 		// Building notification
 		var b strings.Builder
 
-		if multisig.Safe == multisig.To {
-			b.WriteString("Multisignature in *" + subscription.Label + "*\n")
-		} else {
+		isOutcoming := multisig.Safe != multisig.To
+		isDelegateCall := multisig.Operation == 1
+
+		// Checking for overlapping transactions with same nonce
+		// If there is, first executed with deny others
+		ordering := "modified"
+		modified := multisig.Modified.Format(time.RFC3339)
+		confictMultisig, err := client.MultisigTransactions(subscription.SafeAddress, gnosis.MultisigOptions{
+			Ordering:   &ordering,
+			ModifiedLt: &modified,
+			Nonce:      &multisig.Nonce,
+		})
+
+		// Is it new multisignature transaction?
+		// When add "proposed by" to header
+		// ! DOESNT WORK PROPERLY
+		// ? Maybe round to seconds
+		isNew := multisig.SubmissionDate.Equal(multisig.Modified)
+		if isNew {
+			proposedBy := multisig.Confirmations[0].Owner
+			link := escape.Markdown.Replace(gnosis.ExplorerLink(network, proposedBy))
+			if !isOutcoming && len(confictMultisig.Results) > 0 {
+				b.WriteString("On-chain rejection proposed by:\n")
+			} else {
+				b.WriteString("Proposed by:\n")
+			}
+			b.WriteString(fmt.Sprintf("[%s](%s)\n", proposedBy, link))
+			b.WriteByte('\n')
+		}
+
+		service.logger.Info("tm", zap.Time("s", multisig.SubmissionDate), zap.Time("m", multisig.Modified))
+		service.logger.Info("is", zap.Bool("new", isNew), zap.Bool("out", isOutcoming))
+
+		// Notification headder, with info about from and to
+		if isDelegateCall {
+			b.WriteString("Delegate call in *" + subscription.Label + "*\n")
+		} else if isOutcoming {
 			b.WriteString(fmt.Sprintf("From: *%s*\n", subscription.Label))
 
 			var verb string
@@ -76,6 +110,7 @@ func (service *Service) FormatMultisigTransactions(subscription models.Subscript
 				verb = "Send"
 			}
 
+			// Formatting ethereum value
 			value := new(big.Int)
 			value, ok := value.SetString(multisig.Value, 10)
 			if !ok {
@@ -87,10 +122,13 @@ func (service *Service) FormatMultisigTransactions(subscription models.Subscript
 			link := escape.Markdown.Replace(gnosis.ExplorerLink(network, multisig.To))
 			b.WriteString(fmt.Sprintf("%s *%s ETH* to:\n", verb, escape.Markdown.Replace(eth.String())))
 			b.WriteString(fmt.Sprintf("[%s](%s)\n", multisig.To, link))
+		} else {
+			b.WriteString("Multisignature in *" + subscription.Label + "*\n")
 		}
 
 		b.WriteByte('\n')
 
+		// Checking status, is it executed, is all confirmations collected?
 		remain := safe.Threshold - int64(len(multisig.Confirmations))
 		thresholdReached := remain == 0
 		if multisig.ConfirmationsRequired == nil {
@@ -107,13 +145,61 @@ func (service *Service) FormatMultisigTransactions(subscription models.Subscript
 
 		b.WriteByte('\n')
 
-		b.WriteString("Confirmations:\n")
-		for _, conf := range multisig.Confirmations {
-			link := escape.Markdown.Replace(gnosis.ExplorerLink(network, conf.Owner))
-			b.WriteString(fmt.Sprintf("[%s](%s)\n", conf.Owner, link))
-			b.WriteString("\t\t— Signed " + humantime.Since(conf.SubmissionDate) + "\n")
+		if !isNew {
+			b.WriteString("Confirmations:\n")
+			for _, conf := range multisig.Confirmations {
+				link := escape.Markdown.Replace(gnosis.ExplorerLink(network, conf.Owner))
+				b.WriteString(fmt.Sprintf("[%s](%s)\n", conf.Owner, link))
+				b.WriteString("\t\t— Signed " + humantime.Since(conf.SubmissionDate) + "\n")
+			}
 		}
 
+		if err == nil && len(confictMultisig.Results) > 0 {
+			b.WriteByte('\n')
+
+			b.WriteString("Conficts:\n")
+
+			for _, conflict := range confictMultisig.Results {
+				isOutcoming := conflict.Safe != conflict.To
+				isDelegateCall := conflict.Operation == 1
+				remain := safe.Threshold - int64(len(conflict.Confirmations))
+				isThresholdReached := remain == 0
+
+				b.WriteString("\t")
+				if isDelegateCall {
+					b.WriteString("Delegate call:\n")
+				} else if isOutcoming {
+					// Formatting ethereum value?
+					value := new(big.Int)
+					value, ok := value.SetString(multisig.Value, 10)
+					if !ok {
+						service.logger.Error("failed to parse multisig value")
+						continue
+					}
+					eth := toDecimal(value, defaultEtherDecimals)
+
+					b.WriteString(fmt.Sprintf("Send *%s ETH* to:\n", escape.Markdown.Replace(eth.String())))
+				} else {
+					b.WriteString("Multisignature:\n")
+				}
+
+				// Link to overlapping transaction
+				link := escape.Markdown.Replace(fmt.Sprintf("https://gnosis-safe.io/app/%[1]s:%[2]s/transactions/multisig_%[2]s_%[3]s",
+					network.AsPrefix(), safe.Address, conflict.SafeTxHash))
+				b.WriteString(fmt.Sprintf("[%s](%s)\n", conflict.To, link))
+
+				confirmations := fmt.Sprintf("%d/%d", len(conflict.Confirmations), safe.Threshold)
+				if isThresholdReached {
+					b.WriteString("\t\t— " + confirmations + " Ready for execution " + humantime.Since(conflict.Modified) + "\n")
+				} else {
+					b.WriteString("\t\t— " + confirmations + " Last confirmation " + humantime.Since(conflict.Modified) + "\n")
+				}
+			}
+		} else {
+			service.logger.Error("failed to get overlapping multisignature transactions", zap.Int64("nonce", multisig.Nonce), zap.Error(err))
+		}
+
+		// Adding url button to transaction for fast access to confirmation
 		menu := tg.ReplyMarkup{}
 		btn := menu.URL("Gnosis",
 			fmt.Sprintf("https://gnosis-safe.io/app/%[1]s:%[2]s/transactions/multisig_%[2]s_%[3]s",
